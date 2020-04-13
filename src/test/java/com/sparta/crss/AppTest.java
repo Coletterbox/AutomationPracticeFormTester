8ae64d82ee6fcd0dcef81fcba531175a15c34ec9
Colette Shaw <CShaw@spartaglobal.com>
2020-04-13 22:30:56 +0100

    Add to HomePage; add test page

--- src/test/java/com/sparta/crss/AppTest.java
+++ src/test/java/com/sparta/crss/AppTest.java
@@ -1,20 +1,30 @@
 package com.sparta.crss;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    static WebDriver webDriver = new ChromeDriver();
+
+    @BeforeAll
+    public static void setup() {
+        new HomePage(webDriver);
     }
+
+    @Test
+    public void testTest()
+    {
+        assertEquals("test", "test");
+    }
+
+
 }
--- src/test/java/com/sparta/crss/HomePage.java
+++ src/test/java/com/sparta/crss/HomePage.java
@@ -1,4 +1,24 @@
 package com.sparta.crss;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    WebDriver webDriver;
+    String link = "http://localhost:9292/";
+
+    public HomePage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        setUp(webDriver);
+        webDriver.get(link);
+    }
+
+    public void setUp(WebDriver webDriver) {
+        webDriver.manage().window().maximize();
+    }
+
+    public static String getUrl(WebDriver webDriver, String text) {
+        webDriver.findElement(By.linkText(text)).click();
+        return webDriver.getCurrentUrl();
+    }
 }
