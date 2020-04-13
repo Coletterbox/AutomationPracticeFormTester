7b9e1613ee9a6cdbbb69683af9b01526c0c4ccce
Colette Shaw <CShaw@spartaglobal.com>
2020-04-13 23:52:58 +0100

    Fix imports

--- src/test/java/com/sparta/crss/AppTest.java
+++ src/test/java/com/sparta/crss/AppTest.java
@@ -1,19 +1,30 @@
 package com.sparta.crss;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import org.junit.Ignore;
+//import org.junit.Test;
+//import org.junit.jupiter.api.BeforeAll;
 
 import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import org.junit.jupiter.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.net.MalformedURLException;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
     static WebDriver webDriver = new ChromeDriver();
 
