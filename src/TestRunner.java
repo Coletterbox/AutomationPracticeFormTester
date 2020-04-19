eb20d928b1d06436ffab7778d3ee7dda7262a69a
Colette Shaw <cshaw@spartaglobal.com>
2020-04-19 20:54:31 +0100

    Add TestRunner

---
+++ src/TestRunner.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features", glue={"C:\\Users\\Colette Shaw\\AutomationPracticeFormTester\\src\\test\\java\\MyStepdefs.java"})
+public class Runner {
+
+}
