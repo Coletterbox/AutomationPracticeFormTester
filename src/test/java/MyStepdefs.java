5a19898b38a97f52dc7478c17846aba5c4be97f2
Colette Shaw <cshaw@spartaglobal.com>
2020-04-19 20:28:53 +0100

    Add first stepDef

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -1,12 +1,26 @@
 import com.sparta.crss.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyStepdefs {
+    WebDriver webDriver = new ChromeDriver();
+
+    @Given("I am on the form page")
+    public void iAmOnTheFormPage() {
+        //this is already a given
+    }
+
     @When("I type into the first name box")
     public void iTypeIntoTheFirstNameBox() {
-        WebDriver webDriver = new ChromeDriver();
         HomePage.enterTextIntoBox(webDriver, "firstName", "Bob");
+    }
+
+    @Then("the text will be in the first name box")
+    public void theTextWillBeInTheFirstNameBox() {
+        assertEquals(HomePage.getTextFromBox(webDriver, "firstName"), "Bob");
     }
 }
